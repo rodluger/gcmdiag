596644773a831d32af8b93981766b10f2ca909a1
Rodrigo Luger <rodluger@gmail.com>
2016-11-03 08:42:12 +0000

    dir

--- gcmdiag/__init__.py
+++ gcmdiag/__init__.py
@@ -8,3 +8,4 @@ from .rect import *
 
 import os
 GCMDIAG_SRC = os.path.dirname(os.path.abspath(__file__))
+GCMDIAG_OUT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'output')
