8aa257a9c255d5e5ad7c24f976d3c7502bd5d35e
Rodrigo Luger <rodluger@gmail.com>
2016-11-08 12:26:21 +0000

    added diag

--- scripts/compare_toa_mse.py
+++ scripts/compare_toa_mse.py
@@ -19,20 +19,15 @@ for run in ['1bar', 'p5bar', 'p25bar']:
   mse = data.convintmeridflux
   
   # Plot
-  vmin = np.min([toa.min(), mse.min()])
-  vmax = np.max([toa.max(), mse.max()])
-  fig = pl.figure(figsize = (14, 10))
+  fig, ax = pl.subplots(1, 2, figsize = (14, 10))
+  ax = ax.flatten()
   fig.subplots_adjust(top = 0.875)
-  ax = [pl.subplot2grid((1, 11 * 2 + 1), (0, 11 * n), colspan = 10) for n in range(2)]
-  cax = pl.subplot2grid((1, 11 * 2 + 1), (0, 11 * 2))
 
-  gcm.ColorMap(x, y, toa, ax = ax[0], cax = cax, vmin = vmin, vmax = vmax)
+  gcm.ColorMap(x, y, toa, ax = ax[0])
   ax[0].set_title('TOA imbalance')
   
-  gcm.ColorMap(x, y, mse, ax = ax[1], cax = cax, vmin = vmin, vmax = vmax)
+  gcm.ColorMap(x, y, mse, ax = ax[1])
   ax[1].set_title('ConvIntMeridFlux')
-  ax[1].set_ylabel('')
-  ax[1].set_yticklabels([])
   
   pl.suptitle('Energy conservation', fontsize = 18)
   fig.savefig('images/compare_toa_mse_%s.png' % run)
