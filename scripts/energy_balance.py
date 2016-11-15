01ba1f11108712a713cf3b22adf9be2dba87c733
Rodrigo Luger <rodluger@gmail.com>
2016-11-15 14:43:24 +0000

    better plots

--- scripts/energy_balance.py
+++ scripts/energy_balance.py
@@ -5,9 +5,17 @@ import os
 import matplotlib.pyplot as pl
 import numpy as np
 
-# Do all three original runs
-for run in ['1bar', 'p5bar', 'p25bar']:
+# The runs we'll plot
+runs = ['1bar', 'p5bar', 'p25bar']
 
+# Plot
+fig, ax = pl.subplots(3, 3, figsize = (12, 12))
+fig.subplots_adjust(wspace = 0.05, hspace = 0.075)
+
+# Do all three original runs
+for i, run in enumerate(runs):
+  
+  '''
   # Load
   print("Running %s..." % run)
   file = os.path.join(GCMDIAG_OUT, 'N2-%s/day1000h00/day1000h00.atmos_daily.nc' % run)
@@ -22,44 +30,40 @@ for run in ['1bar', 'p5bar', 'p25bar']:
   efg_mean = efg - efg_eddy
   efg_dry = data.energy_flux_gradient(dry = True, latent = False)
   efg_latent = data.energy_flux_gradient(dry = False, latent = True)
-  
-  # Plot
-  fig, ax = pl.subplots(1, 3, figsize = (16, 6))
-  fig.subplots_adjust(wspace = 0.05)
-  ax = ax.flatten()
+  '''
   
   # Energy balance
-  ax[0].plot(y, toa, 'r-', lw = 2, label = 'TOA')
-  ax[0].plot(y, efg, 'b-', lw = 2, label = r'$\nabla\Phi$')
-  ax[0].set_xlabel('Latitude (deg)')
-  ax[0].set_xlim(-90, 90)
-  ax[0].set_ylabel('Energy flux (W/m^2)')
-  ax[0].legend(loc = 'upper left')
-  ax[0].set_title('Mean and eddy')
+  #ax[i,0].plot(y, toa, 'r-', lw = 2, label = 'TOA')
+  #ax[i,0].plot(y, efg, 'b-', lw = 2, label = r'$\nabla\Phi$')
+  ax[i,0].set_xlim(-90, 90)
+  if i < 2: ax[i,0].set_xticklabels([])
+  ax[i,0].set_ylabel(r'Energy flux (W/m$^2$)')
+  ax[i,0].legend(loc = 'upper left')
   
   # Mean and eddy
-  ax[1].plot(y, efg_eddy, 'r-', lw = 2, alpha = 0.75, label = r'$\nabla\Phi_\mathrm{eddy}$')
-  ax[1].plot(y, efg_mean, 'b-', lw = 2, alpha = 0.75, label = r'$\nabla\Phi_\mathrm{mean}$')
-  ax[1].set_xlabel('Latitude (deg)')
-  ax[1].set_xlim(-90, 90)
-  ax[1].set_ylabel('')
-  ax[1].set_yticklabels([])
-  ax[1].legend(loc = 'upper left')
-  ax[1].set_title('Mean and eddy')
+  #ax[i,1].plot(y, efg_eddy, 'r-', lw = 2, alpha = 0.75, label = r'$\nabla\Phi_\mathrm{eddy}$')
+  #ax[i,1].plot(y, efg_mean, 'b-', lw = 2, alpha = 0.75, label = r'$\nabla\Phi_\mathrm{mean}$')
+  ax[i,1].set_xlim(-90, 90)
+  ax[i,1].set_yticklabels([])
+  if i < 2: ax[i,1].set_xticklabels([])
+  ax[i,1].legend(loc = 'upper left')
   
   # Dry and latent
-  ax[2].plot(y, efg_dry, 'r-', lw = 2, alpha = 0.75, label = r'$\nabla\Phi_\mathrm{dry}$')
-  ax[2].plot(y, efg_latent, 'b-', lw = 2, alpha = 0.75, label = r'$\nabla\Phi_\mathrm{latent}$')
-  ax[2].set_xlabel('Latitude (deg)')
-  ax[2].set_xlim(-90, 90)
-  ax[2].set_ylabel('')
-  ax[2].set_yticklabels([])
-  ax[2].legend(loc = 'upper left')
-  ax[2].set_title('Latent and dry')
+  #ax[i,2].plot(y, efg_dry, 'r-', lw = 2, alpha = 0.75, label = r'$\nabla\Phi_\mathrm{dry}$')
+  #ax[i,2].plot(y, efg_latent, 'b-', lw = 2, alpha = 0.75, label = r'$\nabla\Phi_\mathrm{latent}$')
+  ax[i,2].set_xlim(-90, 90)
+  ax[i,2].set_yticklabels([])
+  if i < 2: ax[i,2].set_xticklabels([])
+  ax[i,2].legend(loc = 'upper left')
   
-  # Scale all plots the same
-  [axis.axhline(0, color = 'k', alpha = 0.5, zorder = -99, ls = '--') for axis in ax]
-  [axis.set_ylim(-200, 250) for axis in ax]
-  
-  pl.suptitle('Energy conservation', fontsize = 18)
-  fig.savefig('images/energy_balance_%s.png' % run)
+# Appearance
+ax[0,0].set_title('Energy balance')
+ax[0,1].set_title('Mean and eddy')
+ax[0,2].set_title('Latent and dry')
+[ax[n,0].text(-0.4, 0.5, runs[n], rotation = 90, fontsize = 16, transform = ax[n,0].transAxes, ha = 'center', va = 'center') for n in range(3)]
+[ax[-1,n].set_xlabel('Latitude (deg)') for n in range(3)]
+[axis.axhline(0, color = 'k', alpha = 0.5, zorder = -99, ls = '--') for axis in ax.flatten()]
+[axis.set_ylim(-200, 250) for axis in ax.flatten()]
+
+# Save
+fig.savefig('images/energy_balance.png')
